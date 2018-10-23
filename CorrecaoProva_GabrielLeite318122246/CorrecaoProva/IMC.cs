0648ea248e01c586812498920a30a71aca6f1bef
gabrielleite16 <36733599+gabrielleite16@users.noreply.github.com>
2018-10-23 20:29:29 -0300

    Add files via upload

---
+++ CorrecaoProva_GabrielLeite318122246/CorrecaoProva/IMC.cs
@@ -0,0 +1,50 @@
+﻿namespace CorrecaoProva_GabrielLeite318122246
+{
+     class IMC
+    {
+        private double peso;
+        private double altura;
+
+        public IMC()
+        {
+            this.peso = 0.0;
+            this.altura = 0.0;
+        }
+
+        public IMC(double peso, double altura)
+        {
+            this.peso = peso;
+            this.altura = altura;
+        }
+
+        public double Calcular(double peso, double altura)
+        {
+            double retorno = (peso / (altura * altura));
+            return retorno;
+        }
+
+        public string Classificar(double imc)
+        {
+            string classificacao = "Não validado!";
+
+            if (imc < 16)
+                classificacao = "Magreza Grave!";
+            else if (imc > 16 & imc <= 18.4)
+                classificacao = "Magreza Moderada!";
+            else if (imc >= 18.5 && imc <= 24)
+                classificacao = "Magreza Leve!";
+            else if (imc >= 25 && imc <= 30)
+                classificacao = "Saudável!";
+            else if (imc >= 31 && imc <= 34)
+                classificacao = "Obesidade GRAU I";
+            else if (imc >= 35 && imc <= 39)
+                classificacao = "Obesidade GRAU II";
+            else if (imc > 40)
+                classificacao = "Obesidade GRAU III";
+
+            return classificacao;
+        }
+    }
+}
+    }
+}
---
+++ CorrecaoProva_GabrielLeite318122246/CorrecaoProva/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorrecaoProva_GabrielLeite318122246
+{
+    class Program
+    {
+
+        static string ComunicacaoComClasse()
+        {
+            IMC imc = new IMC();
+            Console.WriteLine("Informe seu peso: ");
+            double peso = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe sua altura: ");
+            double altura = double.Parse(Console.ReadLine());
+
+            string classificacao = imc.Classificar(imc.Calcular(peso, altura));
+
+            return classificacao;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculo de IMC: ");
+            Console.WriteLine("Sua classificação é {0}", ComunicacaoComClasse());
+
+            Console.ReadKey();
+        }
+    }
+}
